c299ee81c8962f07021d040e9cdb56f2546823a1
PudenceAhimbisibwe <prudeahimbisibwe@gmail.com>
2022-02-18 23:12:35 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,28 @@
+import kotlin.math.min
+
+fun main(){
+    Name()
+    modulus(15,4)
+    addition(7,8,3,9)
+    interestingfact()
+}
+// question1
+fun Name(){
+    var name ="Ahimbisibwe prudence"
+    println("Hello "+name)
+}
+// question2
+fun modulus(b: Int, c: Int){
+    var result= b%c
+    println(result)
+    }
+// question3
+    fun addition(b:Int,c:Int,d:Int,e:Int) {
+        var result = b+c+d+e
+        println(result)
+    }
+// question
+fun interestingfact(){
+    var interesting= "I Love God"
+    println(interesting)
+}
